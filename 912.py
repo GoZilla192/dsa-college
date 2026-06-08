21a183406351c735302be6308d38c04935f48623
gozilla <wpdr5r@gmail.com>
2026-06-08 20:06:22 +0300

    -

--- 912.py
+++ 912.py
@@ -1,1 +1,12 @@
-# todo: write quick sort
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+
+        else:
+            mid_idx = len(nums) // 2
+            mid = nums.pop(mid_idx)
+            left = [x for x in nums if x <= mid]
+            right = [x for x in nums if x > mid]
+
+            return self.sortArray(left) + [mid] + self.sortArray(right)
